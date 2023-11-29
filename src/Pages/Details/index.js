28f9f69fe4ce6abf2664fb2a84a96805e2b54354
beyzaerkan <beyzaerkan@icloud.com>
2023-11-29 21:25:39 +0300

    initialize

---
+++ src/Pages/Details/index.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Details = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+};
+
+export default Details;
